d4e46fb164bc95333149783ea2e58898cc7c90a6
fsadriann <adrianfelipeforero@gmail.com>
2025-09-26 14:35:26 -0500

    feat: se añadió el metodo de potenciacion

--- Operaciones.java
+++ Operaciones.java
@@ -1,2 +1,16 @@
 public class Operaciones {
+    public static int potencia(int base, int exponente) {
+        int resultado = 1;
+
+        if (exponente < 0) {
+            System.out.println("Este método solo admite exponentes enteros no negativos.");
+            return -1;
+        }
+
+        for (int i = 0; i < exponente; i++) {
+            resultado *= base;
+        }
+
+        return resultado;
+    }
 }
--- Principal.java
+++ Principal.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Ingrese base y exponente: ");
+            
+            int base = scanner.nextInt();
+            int exponente = scanner.nextInt();
+            int resultado = Operaciones.potencia(base, exponente);
+            System.out.println("Resultado: " + resultado);
+        }
+    }
 }
