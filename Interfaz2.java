af3ea2071f4eb4fee537844a9128626eb17c6af0
Davidasuva <Davidsantiagogonzalez8976@gmail.com>
2025-09-29 21:55:25 -0500

    Interfaz funcionando,solo faltan los metodos de las operaciones

--- Interfaz2.java
+++ Interfaz2.java
@@ -4,6 +4,7 @@ import java.awt.event.*;
 
 public class Interfaz2 extends JFrame {
     private JTextField Texto;
+    LogicaInterfaz logica= new LogicaInterfaz();
 
     public Interfaz2() {
         setTitle("Calculadora");
@@ -11,12 +12,15 @@ public class Interfaz2 extends JFrame {
         setSize(700, 700);
         setLocationRelativeTo(null);
 
-        Texto = new JTextField();
+        Texto = new JTextField(logica.getTexto());
+        Texto.setHorizontalAlignment(SwingConstants.RIGHT);
+        Texto.setEditable(false);
+        Texto.setFocusable(true);
         Texto.setFont(new Font("Arial", Font.BOLD, 30));
         add(Texto, BorderLayout.NORTH);
 
         JPanel botones = new JPanel();
-        botones.setLayout(new GridLayout(5, 4, 6, 6));
+        botones.setLayout(new GridLayout(6, 4, 6, 6));
         add(botones, BorderLayout.CENTER);
 
         String[] teclas = {
@@ -24,19 +28,69 @@ public class Interfaz2 extends JFrame {
                 "4","5","6","-",
                 "7","8","9","*",
                 "C","0","=","/",
-                "X","'","√","%"
+                "X","'","√","%",
+                "."
         };
 
         for(String tecla: teclas){
             JButton boton = new JButton(tecla);
             boton.setFont(new Font("Arial",Font.BOLD,30));
+            boton.addActionListener(this::alClickear);
             botones.add(boton);
         }
 
         setVisible(true);
     }
+    private void alClickear(ActionEvent ev){
+        String tecla=((JButton)ev.getSource()).getText();
+        switch(tecla){
+            default:
+                if(tecla.matches("\\d")){
+                    logica.addNumero(tecla);
+                    break;
+                }
+            case "√":
+                logica.operacion(tecla);
+                break;
+            case "%":
+                logica.operacion(tecla);
+                break;
+            case "*":
+                logica.operacion(tecla);
+                break;
+            case "'":
+                logica.operacion(tecla);
+                break;
+            case "/":
+                logica.operacion(tecla);
+                break;
+            case "-":
+                logica.operacion(tecla);
+                break;
+            case "+":
+                logica.operacion(tecla);
+                break;
+            case ".":
+                logica.addDecimal();
+                break;
+            case "=":
+                logica.igual();
+                break;
+            case "C":
+                logica.reinicio();
+                break;
+            case "X":
+                logica.borrar();
+                break;
+        }
+        actualizarTexto();
+    }
+
+    private void actualizarTexto(){
+        Texto.setText(logica.getTexto());
+    }
 
     public static void main(String[] args) {
-        new Interfaz2();
+        SwingUtilities.invokeLater(Interfaz2::new);
     }
 }
