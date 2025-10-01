3b96f204425dd5fadcdf7521256d1f7b902d0761
camilio77 <156365533+camilio77@users.noreply.github.com>
2025-10-01 12:46:31 -0500

    Clase principal de rama test modificada

--- Principal.java
+++ Principal.java
@@ -1,15 +1,5 @@
-import java.util.Scanner;
-
 public class Principal {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Ingrese base y exponente: ");
-            
-            int base = scanner.nextInt();
-            int exponente = scanner.nextInt();
-            int resultado = Operaciones.potencia(base, exponente);
-            System.out.println("Resultado: " + resultado);
-        }
         Operaciones operacion=new Operaciones();
     }
 }
