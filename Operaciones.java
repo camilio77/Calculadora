4cfae19fe1f91a5017e346e1ef1c0dfa9a597f6d
Juan Gamboa <gamboajuandavid2005@gmail.com>
2025-10-01 11:32:03 -0500

    Update Operaciones.java
    Se implemento la función de divisiones en la clase Operaciones

--- Operaciones.java
+++ Operaciones.java
@@ -1,2 +1,25 @@
 public class Operaciones {
+    public static int potencia(int base, int exponente) {
+        int resultado = 1;
+
+        if (exponente < 0) {
+            System.out.println("Este método solo admite exponentes enteros no negativos.");
+            return -1;
+        }
+
+        for (int i = 0; i < exponente; i++) {
+            resultado *= base;
+        }
+
+        return resultado;
+    }
+
+    public static double division(int dividendo, int divisor) {
+        if (divisor == 0) {
+            System.out.println("Error: no se puede dividir entre cero.");
+            return -1; // Valor especial para indicar error
+        }
+
+        return (double) dividendo / divisor;
+    }
 }
