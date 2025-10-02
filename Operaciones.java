c3407a2d2d2652d98e503592b482d33aa7cda677
juanse1172 <juanseruve28@gmail.com>
2025-10-02 12:08:34 -0500

    Operaciones resta, division y multiplicacion agregadas

--- Operaciones.java
+++ Operaciones.java
@@ -1,2 +1,13 @@
 public class Operaciones {
+    public static double multiplicar(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double dividir(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    public static double restar(double num1, double num2) {
+        return num1 - num2;
+    }
 }
